9e93824b93396f57c72a827b5b5f47ba8090aa11
Johanny Tavarez <scarlet.tavarezg@gmail.com>
2021-11-30 12:12:23 -0400

    Campos de registro

--- AppReconocimientoFacial/Registro.Designer.cs
+++ AppReconocimientoFacial/Registro.Designer.cs
@@ -29,48 +29,112 @@ namespace AppReconocimientoFacial
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.txtNombre = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.txtEdad = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
+            this.txtTelefono = new System.Windows.Forms.Panel();
+            this.CBNacionalidad = new System.Windows.Forms.ComboBox();
             this.label4 = new System.Windows.Forms.Label();
             this.txtCedula = new System.Windows.Forms.TextBox();
-            this.CBNacionalidad = new System.Windows.Forms.ComboBox();
-            this.panel1.SuspendLayout();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.CBTipoPiel = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.checkedListBox2 = new System.Windows.Forms.CheckedListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label12 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtTelefono.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // panel1
+            // txtTelefono
             // 
-            this.panel1.Controls.Add(this.CBNacionalidad);
-            this.panel1.Controls.Add(this.label4);
-            this.panel1.Controls.Add(this.txtCedula);
-            this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.txtEdad);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.txtNombre);
-            this.panel1.Location = new System.Drawing.Point(362, 1);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(430, 445);
-            this.panel1.TabIndex = 0;
+            this.txtTelefono.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.txtTelefono.Controls.Add(this.button1);
+            this.txtTelefono.Controls.Add(this.btnCancelar);
+            this.txtTelefono.Controls.Add(this.btnGuardar);
+            this.txtTelefono.Controls.Add(this.comboBox2);
+            this.txtTelefono.Controls.Add(this.label11);
+            this.txtTelefono.Controls.Add(this.label10);
+            this.txtTelefono.Controls.Add(this.txtEmail);
+            this.txtTelefono.Controls.Add(this.label9);
+            this.txtTelefono.Controls.Add(this.textBox1);
+            this.txtTelefono.Controls.Add(this.comboBox1);
+            this.txtTelefono.Controls.Add(this.label8);
+            this.txtTelefono.Controls.Add(this.checkedListBox2);
+            this.txtTelefono.Controls.Add(this.label7);
+            this.txtTelefono.Controls.Add(this.CBTipoPiel);
+            this.txtTelefono.Controls.Add(this.label6);
+            this.txtTelefono.Controls.Add(this.checkedListBox1);
+            this.txtTelefono.Controls.Add(this.label5);
+            this.txtTelefono.Controls.Add(this.CBNacionalidad);
+            this.txtTelefono.Controls.Add(this.label4);
+            this.txtTelefono.Controls.Add(this.txtCedula);
+            this.txtTelefono.Controls.Add(this.label3);
+            this.txtTelefono.Controls.Add(this.label2);
+            this.txtTelefono.Controls.Add(this.txtEdad);
+            this.txtTelefono.Controls.Add(this.label1);
+            this.txtTelefono.Controls.Add(this.txtNombre);
+            this.txtTelefono.Location = new System.Drawing.Point(265, 1);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(527, 445);
+            this.txtTelefono.TabIndex = 0;
+            this.txtTelefono.Paint += new System.Windows.Forms.PaintEventHandler(this.txtTelefono_Paint);
             // 
-            // txtNombre
+            // CBNacionalidad
             // 
-            this.txtNombre.Location = new System.Drawing.Point(32, 48);
-            this.txtNombre.Name = "txtNombre";
-            this.txtNombre.Size = new System.Drawing.Size(100, 20);
-            this.txtNombre.TabIndex = 0;
+            this.CBNacionalidad.FormattingEnabled = true;
+            this.CBNacionalidad.Items.AddRange(new object[] {
+            "Dominicana",
+            "Venezolana",
+            "Estadounidense",
+            "Haitiana",
+            "Chilena",
+            "Colombiana",
+            "Puertoriqueña"});
+            this.CBNacionalidad.Location = new System.Drawing.Point(285, 106);
+            this.CBNacionalidad.Name = "CBNacionalidad";
+            this.CBNacionalidad.Size = new System.Drawing.Size(190, 21);
+            this.CBNacionalidad.TabIndex = 8;
             // 
-            // label1
+            // label4
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(29, 32);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Nombre";
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(282, 32);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Cedula";
+            // 
+            // txtCedula
+            // 
+            this.txtCedula.Location = new System.Drawing.Point(285, 48);
+            this.txtCedula.Name = "txtCedula";
+            this.txtCedula.Size = new System.Drawing.Size(190, 20);
+            this.txtCedula.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(282, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Nacionalidad";
             // 
             // label2
             // 
@@ -88,56 +152,226 @@ namespace AppReconocimientoFacial
             this.txtEdad.Size = new System.Drawing.Size(100, 20);
             this.txtEdad.TabIndex = 2;
             // 
-            // label3
+            // label1
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(29, 157);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(69, 13);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Nacionalidad";
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nombre";
             // 
-            // label4
+            // txtNombre
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(29, 227);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(40, 13);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "Cedula";
+            this.txtNombre.Location = new System.Drawing.Point(32, 48);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(233, 20);
+            this.txtNombre.TabIndex = 0;
             // 
-            // txtCedula
+            // label5
             // 
-            this.txtCedula.Location = new System.Drawing.Point(32, 243);
-            this.txtCedula.Name = "txtCedula";
-            this.txtCedula.Size = new System.Drawing.Size(100, 20);
-            this.txtCedula.TabIndex = 6;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(348, 254);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Enfermedades";
             // 
-            // CBNacionalidad
+            // checkedListBox1
             // 
-            this.CBNacionalidad.FormattingEnabled = true;
-            this.CBNacionalidad.Location = new System.Drawing.Point(32, 173);
-            this.CBNacionalidad.Name = "CBNacionalidad";
-            this.CBNacionalidad.Size = new System.Drawing.Size(121, 21);
-            this.CBNacionalidad.TabIndex = 8;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Diabetes",
+            "Alergias",
+            "Ninguna"});
+            this.checkedListBox1.Location = new System.Drawing.Point(351, 278);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(124, 94);
+            this.checkedListBox1.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(29, 254);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Tipo de piel";
+            // 
+            // CBTipoPiel
+            // 
+            this.CBTipoPiel.FormattingEnabled = true;
+            this.CBTipoPiel.Items.AddRange(new object[] {
+            "Grasa",
+            "Mixta",
+            "Seca"});
+            this.CBTipoPiel.Location = new System.Drawing.Point(32, 278);
+            this.CBTipoPiel.Name = "CBTipoPiel";
+            this.CBTipoPiel.Size = new System.Drawing.Size(121, 21);
+            this.CBTipoPiel.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(194, 254);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(99, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Condicion de la piel";
+            // 
+            // checkedListBox2
+            // 
+            this.checkedListBox2.FormattingEnabled = true;
+            this.checkedListBox2.Items.AddRange(new object[] {
+            "Acne",
+            "Manchas",
+            "Arrugas",
+            "Imperfecciones"});
+            this.checkedListBox2.Location = new System.Drawing.Point(197, 280);
+            this.checkedListBox2.Name = "checkedListBox2";
+            this.checkedListBox2.Size = new System.Drawing.Size(120, 94);
+            this.checkedListBox2.TabIndex = 14;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Blanca",
+            "Morena",
+            "Mulata"});
+            this.comboBox1.Location = new System.Drawing.Point(32, 353);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(29, 320);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(66, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Tono de piel";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(312, 151);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(49, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Telefono";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(315, 167);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(160, 20);
+            this.textBox1.TabIndex = 17;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(29, 151);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(32, 13);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "Email";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(32, 167);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(270, 20);
+            this.txtEmail.TabIndex = 19;
+            this.txtEmail.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Femenino",
+            "Masculino"});
+            this.comboBox2.Location = new System.Drawing.Point(153, 106);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(112, 21);
+            this.comboBox2.TabIndex = 22;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(150, 90);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(42, 13);
+            this.label11.TabIndex = 21;
+            this.label11.Text = "Genero";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(302, 412);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 23;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(400, 412);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 24;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(176, 412);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 23);
+            this.button1.TabIndex = 25;
+            this.button1.Text = "Capturar rostro";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(85, 257);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(102, 13);
+            this.label12.TabIndex = 1;
+            this.label12.Text = "Nombre del negocio";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(56, 109);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(142, 113);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
             // 
             // Registro
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.txtTelefono);
             this.Name = "Registro";
             this.Text = "Registro";
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.txtTelefono.ResumeLayout(false);
+            this.txtTelefono.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel txtTelefono;
         private System.Windows.Forms.ComboBox CBNacionalidad;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox txtCedula;
@@ -146,5 +380,24 @@ namespace AppReconocimientoFacial
         private System.Windows.Forms.TextBox txtEdad;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.CheckedListBox checkedListBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox CBTipoPiel;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- AppReconocimientoFacial/Registro.cs
+++ AppReconocimientoFacial/Registro.cs
@@ -16,5 +16,20 @@ namespace AppReconocimientoFacial
         {
             InitializeComponent();
         }
+
+        private void txtTelefono_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
