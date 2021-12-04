10193428653097e56c216c3016ef725ba14ee91a
imXandersss <bbociodiaz@gmail.com>
2021-12-03 20:13:58 -0400

    Formulario de informacion del cliente

---
+++ AppReconocimientoFacial/InformacionCliente.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace AppReconocimientoFacial
+{
+    partial class InformacionCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEnfermedad = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtNota = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.fechaPicker = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(27, 96);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(371, 22);
+            this.txtNombre.TabIndex = 0;
+            this.txtNombre.TextChanged += new System.EventHandler(this.txtNombre_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "DATOS DEL CLIENTE";
+            // 
+            // txtEnfermedad
+            // 
+            this.txtEnfermedad.Location = new System.Drawing.Point(27, 242);
+            this.txtEnfermedad.Multiline = true;
+            this.txtEnfermedad.Name = "txtEnfermedad";
+            this.txtEnfermedad.Size = new System.Drawing.Size(371, 59);
+            this.txtEnfermedad.TabIndex = 10;
+            this.txtEnfermedad.TextChanged += new System.EventHandler(this.txtEnfermedad_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 17);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Nombre";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 139);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 17);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Fecha de cita";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 209);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(116, 17);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Condicion de piel";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(24, 318);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 17);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Enfermedad";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(27, 338);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(371, 38);
+            this.textBox1.TabIndex = 16;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtNota
+            // 
+            this.txtNota.Location = new System.Drawing.Point(27, 419);
+            this.txtNota.Multiline = true;
+            this.txtNota.Name = "txtNota";
+            this.txtNota.Size = new System.Drawing.Size(371, 85);
+            this.txtNota.TabIndex = 18;
+            this.txtNota.TextChanged += new System.EventHandler(this.txtNota_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(24, 388);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 17);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Nota";
+            // 
+            // fechaPicker
+            // 
+            this.fechaPicker.Location = new System.Drawing.Point(27, 173);
+            this.fechaPicker.Name = "fechaPicker";
+            this.fechaPicker.Size = new System.Drawing.Size(371, 22);
+            this.fechaPicker.TabIndex = 19;
+            this.fechaPicker.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // InformacionCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(432, 552);
+            this.Controls.Add(this.fechaPicker);
+            this.Controls.Add(this.txtNota);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtEnfermedad);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNombre);
+            this.Name = "InformacionCliente";
+            this.Text = "E";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEnfermedad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtNota;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DateTimePicker fechaPicker;
+    }
+}
---
+++ AppReconocimientoFacial/InformacionCliente.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppReconocimientoFacial
+{
+    public partial class InformacionCliente : Form
+    {
+        public InformacionCliente()
+        {
+            InitializeComponent();
+        }
+
+        private void txtEnfermedad_TextChanged(object sender, EventArgs e)
+        {
+            txtEnfermedad.ReadOnly = true;
+        }
+
+        private void txtNombre_TextChanged(object sender, EventArgs e)
+        {
+            txtNombre.ReadOnly = true;
+        }
+
+        private void txtApellido_TextChanged(object sender, EventArgs e)
+        {
+            txtApellido.ReadOnly = true;
+        }
+
+        private void txtFecha_TextChanged(object sender, EventArgs e)
+        {
+            txtFecha.ReadOnly = true;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            txtEnfermedad.ReadOnly = true;
+        }
+
+        private void txtNota_TextChanged(object sender, EventArgs e)
+        {
+            txtNota.ReadOnly = true;
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
